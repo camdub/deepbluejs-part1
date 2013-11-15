d45d6c0571330e088833f306f8c3354542748b16
Cameron Woodmansee <cameron29w@gmail.com>
2013-11-15 10:44:13 -0500

    Finished Step 3

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@ jQuery(function($) {
   'use strict';
 
   $.extend(App, {
-    init: function() {}
+    init: function() {
+      this.cacheElements();
+    },
+    cacheElements: function() {
+      this.$todoApp = $('#todoapp');
+      this.$header = this.$todoApp.find('#header');
+      this.$main = this.$todoApp.find('#main');
+      this.$footer = this.$todoApp.find('#footer');
+      this.$newTodo = this.$header.find('#new-todo');
+      this.$toggleAll = this.$main.find('#toggle-all');
+      this.$todoList = this.$main.find('#todo-list');
+      this.$count = this.$footer.find('#todo-count');
+      this.$clearBtn = this.$footer.find('#clear-completed');
+    }
   });
 
   App.init();
